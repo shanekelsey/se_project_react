88acba585330c0529fbb0ac0ec0960803ad2c2eb
ShaneKelsey <Shanekelsey1012@gmail.com>
2026-02-03 12:51:34 -0500

    Update ItemModal

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -1,6 +1,8 @@
 import "./ItemModal.css";
 import closeButton from "../../assets/close-btn-light.png";
 import deleteItem from "../../assets/Delete item.png";
+import { useContext } from "react";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 
 function ItemModal({
   handleCloseModal,
@@ -8,6 +10,8 @@ function ItemModal({
   card,
   isOpen,
 }) {
+  const { currentUser } = useContext(CurrentUserContext);
+  const ownedByCurrentUser = card.owner === currentUser._id;
   return (
     <div className={`modal ${isOpen ? "modal_opened" : ""}`}>
       <div className="modal__content modal__content_type_image">
@@ -22,13 +26,14 @@ function ItemModal({
         <div className="modal__footer">
           <div className="modal__name-delete">
             <h2 className="modal__caption">{card.name}</h2>
-            
+            {ownedByCurrentUser && (
             <button
               onClick={handleDeleteClick}
               className="modal__delete-button"
             >
               <img src={deleteItem} alt="delete-button" />
             </button>
+            )}
           </div>
 
           <p className="modal__weather">Weather: {card.weather}</p>
